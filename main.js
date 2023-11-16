a2cef7b57ef08fded79c8ae7b864b5c6a5d82c35
Tian-Who <cbhomeacademy@gmail.com>
2023-11-16 16:25:03 -0700

    Add  rest

--- main.js
+++ main.js
@@ -1,94 +1,181 @@
-import './style.css'
-import Phaser from 'phaser'
+import "./style.css";
+import Phaser from "phaser";
+
+const TILE_SIZE = 18;
 
 const PLAYER_ANIMS = {
   idle: "idle",
   walk: "walk",
-  run: "run"
-}
+  run: "run",
+  jump: "jump",
+  fall: "fall",
+};
 
 class MainScene extends Phaser.Scene {
   constructor() {
     super("main-scene");
-
+    this.player;
+    this.map;
+    this.keyBinds;
   }
 
   preload() {
-    this.load.atlas("robot", "robot.png", "robot.json"); 
+    this.load.atlas("robot", "robot.png", "robot.json");
 
+    this.load.image("marble", "tilesets/marble.png")
+    this.load.image("rock", "tilesets/rock.png");
+    this.load.image("sand", "tilesets/sand.png");
+    this.load.image("stone", "tilesets/stone.png");
+
+    this.load.tilemapTiledJSON("map", "tilesets/map.json")
   }
-  
+
   create() {
     const { height, width } = this.scale;
 
-    let player = this.physics.add.sprite(width / 2, height / 2, "robot")
-    player.setCollideWorldBounds(true);
+    this.map = this.make.tilemap({key: "map"});
+    this.keyBinds;
+    const marbleTiles = this.map.addTilesetImage("marble", "marble");
+    const rockTiles = this.map.addTilesetImage("rock", "rock");
+    const sandTiles = this.map.addTilesetImage("sand", "sand");
+    const stoneTiles = this.map.addTilesetImage("stone", "stone");
 
-    player.anims.create ({
+   const BackgroundLayer = this.map.createLayer("Background",
+    [marbleTiles, rockTiles, sandTiles, stoneTiles],
+     0, 0
+     );
+   const platformLayer = this.map.createLayer("Platforms",
+    [marbleTiles, rockTiles, sandTiles, stoneTiles],
+     0, 
+     0
+     );
+
+
+
+    platformLayer.setCollisionByProperty({collides: true});
+
+
+
+
+    this.player = this.physics.add.sprite(width / 2,
+     height / 2, 
+     "robot",
+      "character_robot_idle.png");
+
+    this.physics.add.collider(this.player, platformLayer);
+    this.player.setCollideWorldBounds(true)
+    .setBounce(.2)
+    .setSize(TILE_SIZE * 2, TILE_SIZE * 4.5)
+    .setScale(.5)
+    .setOffset(TILE_SIZE * 1.7, TILE_SIZE * 2.6);
+    
+
+
+
+    this.player.anims.create({
       key: "run",
-      frames: player.anims.generateFrameNames("robot", {
+      frames: this.player.anims.generateFrameNames("robot", {
         start: 0,
         end: 2,
         prefix: "character_robot_run",
-        suffix: ".png"
+        suffix: ".png",
       }),
-      frameRate: 10, 
+      
+      frameRate: 10,
       repeat: -1,
+    });
+    this.player.play("run");
+    this.player.setCollideWorldBounds(true);
 
-     });
-     player.play(run);
-     player.setCollideWorldBounds(true);
-     
-     player.setBounce(10)
-  player.anims.create ({
-    key: "walk",
-    frames: player.anims.generateFrameNames("robot", {
-      start: 0,
-      end: 8,
-      prefix: "character_robot_walk",
-      suffix: ".png"
-    }),
-    frameRate: 10, 
-    repeat: -1,
+    this.player.anims.create({
+      key: "walk",
+      frames: this.player.anims.generateFrameNames("robot", {
+        start: 0,
+        end: 7,
+        prefix: "character_robot_walk",
+        suffix: ".png",
+      }),
+      frameRate: 10,
+      repeat: -1,
+    });
+    this.player.play("walk");
 
-   });
-   player.play(walk);
-
-   player.anims.create ({
-    key: "idle",
-    frames: player.anims.generateFrameNames("robot", {
+    this.player.anims.create({
+			key: PLAYER_ANIMS.idle,
+			frames: [{ key: "robot", frame: "character_robot_idle.png" }],
+		});
   
-      prefix: "character_robot_idle",
-      suffix: ".png"
-    }),
+    this.player.anims.create({
+			key: PLAYER_ANIMS.jump,
+			frames: [{ key: "robot", frame: "character_robot_jump.png" }],
+		});
+    this.player.anims.create({
+			key: PLAYER_ANIMS.fall,
+			frames: [{ key: "robot", frame: "character_robot_fall.png" }],
+		});
+    // this.player.play("idle");
 
+    this.keyBinds = this.input.keyboard.addKeys({
+      left: Phaser.Input.Keyboard.KeyCodes.A,
+      leftArrow: Phaser.Input.Keyboard.KeyCodes.LEFT,
+      right: Phaser.Input.Keyboard.KeyCodes.D,
+      rightArrow: Phaser.Input.Keyboard.KeyCodes.RIGHT,
+      jump: Phaser.Input.Keyboard.KeyCodes.SPACE,
+      upArrow: Phaser.Input.Keyboard.KeyCodes.UP,
+      up: Phaser.Input.Keyboard.KeyCodes.W,
+    });
+  }
 
-   });
-   player.play(idle)
-  };
-
-
+  update() {
+    if (this.keyBinds.left.isDown || this.keyBinds.leftArrow.isDown) {
+      this.player.setVelocityX(-150);
+    } else   if (this.keyBinds.right.isDown || this.keyBinds.rightArrow.isDown) {
+      this.player.setVelocityX(150);
+    } else {
+      this.player.setVelocityX(0);
+    }
   
+  if ((this.keyBinds.jump.isDown || this.keyBinds.upArrow.isDown || this.keyBinds.up.isDown) && this.player.body.onFloor()) {
+    this.player.setVelocityY(-150);
+  } 
 
-  update() {}
+  // let x = this.player.body.velocity.x
+  // let y = this.player.body.velocity.y
 
+  let {x, y}= this.player.body.velocity;
+
+  this.player.flipX = x < 0;
+  console.log(x)
+  if(this.player.body.onFloor()){
+    if(x === 0){
+      this.player.play(PLAYER_ANIMS.idle)
+    }else {
+      this.player.play(PLAYER_ANIMS.run, true)
+    }
+  } else{
+    if(y < 0){
+      this.player.play(PLAYER_ANIMS.jump, true)
+    } else {
+      if(y > 0){
+        this.player.play(PLAYER_ANIMS.fall, true)
+    }}
+  }
+}
 }
 
 /**@type {Phaser.Types.Core.GameConfig} */
 const config = {
   type: Phaser.WEBGL,
-  width: 400,
-  height: 400,
+  width: 44 * 18,
+  height: 33 * 18,
   scene: [MainScene],
   physics: {
     default: "arcade",
     arcade: {
-     gravity: {y: 300},
-
-    }
-
-
-  }
+      gravity: { y: 300 },
+      debug:true,
+    },
+  },
 };
 
 const game = new Phaser.Game(config);
---
+++ public/tilesets/marble.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="marble" tilewidth="18" tileheight="18" tilecount="81" columns="9">
+ <image source="marble.png" width="162" height="162"/>
+ <tile id="37" probability="1.5"/>
+ <tile id="40" probability="0.05"/>
+ <tile id="43" probability="0.05"/>
+ <tile id="64" probability="0.1"/>
+ <tile id="67" probability="0.1"/>
+ <tile id="70" probability="0.09"/>
+</tileset>
